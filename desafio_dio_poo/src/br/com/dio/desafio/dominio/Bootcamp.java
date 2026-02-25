d19957ce1acb916c32005eb65a6f52c1e84094d8
Gustavo Soares <gustavosoaresbcge@gmail.com>
2026-02-24 23:37:07 -0300

    feat: implements classes of logic for user

--- desafio_dio_poo/src/br/com/dio/desafio/dominio/Bootcamp.java
+++ desafio_dio_poo/src/br/com/dio/desafio/dominio/Bootcamp.java
@@ -1,5 +1,55 @@
 package br.com.dio.desafio.dominio;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
 public class Bootcamp {
+	private String name;
+	private String description;
+	private final LocalDate inicialDate = LocalDate.now();
+	private final LocalDate finalDate = inicialDate.plusDays(45);
+	private Set<Dev> subscriberDev = new HashSet<>();
+	private Set<Conteudo> contents = new LinkedHashSet<>();
+	
+	public String getName() {return name;}
+	
+	public void setName(String name) {this.name = name;}
+	
+	public String getDescription() {return description;}
+	
+	public void setDescription(String description) {this.description = description;}
+	
+	public Set<Dev> getSubscriberDev() {return subscriberDev;}
+	
+	public void setSubscriberDev(Set<Dev> subscriberDev) {this.subscriberDev = subscriberDev;}
+	
+	public Set<Conteudo> getContents() {return contents;}
+	
+	public void setContents(Set<Conteudo> contents) {this.contents = contents;}
+	
+	public LocalDate getInicialDate() {return inicialDate;}
+	
+	public LocalDate getFinalDate() {return finalDate;}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(contents, description, finalDate, inicialDate, name, subscriberDev);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bootcamp other = (Bootcamp) obj;
+		return Objects.equals(contents, other.contents) && Objects.equals(description, other.description)
+				&& Objects.equals(finalDate, other.finalDate) && Objects.equals(inicialDate, other.inicialDate)
+				&& Objects.equals(name, other.name) && Objects.equals(subscriberDev, other.subscriberDev);
+	}
 
 }
--- desafio_dio_poo/src/br/com/dio/desafio/dominio/Dev.java
+++ desafio_dio_poo/src/br/com/dio/desafio/dominio/Dev.java
@@ -1,5 +1,62 @@
 package br.com.dio.desafio.dominio;
 
-public class Dev {
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 
+public class Dev {
+	private String name;
+	private Set<Conteudo> subscriberContent = new LinkedHashSet<>();
+	private Set<Conteudo> completedContent = new LinkedHashSet<>();
+	
+	public void signUpBootcamp(Bootcamp bootcamp) {
+		this.subscriberContent.addAll(bootcamp.getContents());
+		bootcamp.getSubscriberDev().add(this);
+	}
+	
+	public void makeProgress() {
+		Optional<Conteudo> conteudo = this.subscriberContent.stream().findFirst();
+		if(conteudo.isPresent()) {
+			this.completedContent.add(conteudo.get());
+			this.subscriberContent.remove(conteudo.get());
+		} else {
+			System.err.println("Você não está matriculado em nenhum conteúdo!");
+		}
+	}
+	
+	public double calculateTotalXp() {
+		return this.completedContent.stream().mapToDouble(conteudo -> conteudo.calculateXp()).sum();
+	}
+
+	public String getName() {return name;}
+
+	public void setName(String name) {this.name = name;}
+
+	public Set<Conteudo> getSubscriberContent() {return subscriberContent;}
+
+	public void setSubscriberContent(Set<Conteudo> subscriberContent) {this.subscriberContent = subscriberContent;}
+
+	public Set<Conteudo> getCompletedContent() {return completedContent;}
+
+	public void setCompletedContent(Set<Conteudo> completedContent) {this.completedContent = completedContent;}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(completedContent, name, subscriberContent);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Dev other = (Dev) obj;
+		return Objects.equals(completedContent, other.completedContent) && Objects.equals(name, other.name)
+				&& Objects.equals(subscriberContent, other.subscriberContent);
+	}
+	
 }
